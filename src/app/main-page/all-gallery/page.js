fdb246525c9c5087a50350fab81deceb8c3a734d
Tasleemali <133391205+Tasleemali@users.noreply.github.com>
2025-04-26 14:54:22 +0530

    Update page.js

--- src/app/main-page/all-gallery/page.js
+++ src/app/main-page/all-gallery/page.js
@@ -28,7 +28,7 @@ const Gallery = () => {
           </div>
           <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
             {Array(8).fill(null).map((_, idx) => (
-              <div key={idx} className='aspect-[3/4] bg-gray-300 rounded-md h-60'></div>
+              <div key={idx} className='aspect-[4/4] bg-gray-300 rounded-md h-60'></div>
             ))}
           </div>
 
@@ -46,12 +46,13 @@ const Gallery = () => {
       <div className='max-w-screen-2xl mx-auto px-4 md:px-6 py-8'>
         <h1 className='text-2xl md:text-3xl font-semibold text-center mb-6'>Gallery</h1>
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4'>
-          {gallery.map((glry, idx) => (
-            <div key={idx} className='aspect-[3/4]'>
-              <img src={glry} alt={`Gallery Image ${idx + 1}`} className='w-full h-60 object-cover rounded-md' />
-            </div>
-          ))}
-        </div>
+         {gallery.map((glry ,idx )=> 
+            <div key={idx} className=' aspect-ratio-[3/4] '>
+            <img src={glry} className='w-full h-60  object-cover rounded-xs rounded-bl-4xl rounded-tr-4xl '/>
+          </div>
+        )}
+
+      </div>
         <div className="mt-20">
           <Contact />
         </div>
